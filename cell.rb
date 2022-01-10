24912e9f57e2633bba57bfba48ccc41e154bdfa2
Michael Newman <newman174@gmail.com>
2022-01-10 15:57:27 -0500

    cell

--- cell.rb
+++ cell.rb
@@ -1,15 +1,10 @@
 class Cell
-  attr_accessor :type, :content, :hsh, :heading_level
+  attr_accessor :type, :content, :hash
 
-  def initialize(content: '', type: 'markdown', heading_level: 0)
-    self.hsh = if type == 'code'
-                 make_code_cell(content: content)
-               else
-                 make_md_cell(content: content, heading_level: heading_level)
-               end
+  def initialize(content: '', type: 'code')
+    self.hash = make_cell_hash(content: content)
     self.type = type
     self.content = content
-    self.heading_level = heading_level
   end
   
   # self.make_cell - Valid cell_type are ['code'*, 'markdown'], *default
@@ -22,25 +17,53 @@ class Cell
       "source"=>[content]}
   end
 
-  def make_code_cell(content: '')
-    make_cell_hash(type: 'code', content: content)
-  end
+  # def make_code_cell(content: '')
+  #   make_cell_hash(type: 'code', content: content)
+  # end
 
-  def make_md_cell(content: '', heading_level: 0)
-    if heading_level.positive?
-      hash_marks = '#' * heading_level
-      content = hash_marks + ' ' + content
-    end
+  # def make_md_cell(content: '', heading_level: 0)
+  #   if heading_level.positive?
+  #     hash_marks = '#' * heading_level
+  #     content = hash_marks + ' ' + content
+  #   end
 
-    make_cell_hash(type: 'markdown', content: content)
-  end
+  #   make_cell_hash(type: 'markdown', content: content)
+  # end
 
   def inspect
     details = []
-    details << "#{type} Cell"
-    details << content
-    details << "\n"
+    details << "Type: #{type} Cell"
+    details << "Content:"
+    details << content.lines[0]
+    # details << "\n"
     details
   end
 
+  def to_s
+    content
+  end
+
 end
+
+class CodeCell < Cell; end
+
+class MarkdownCell < Cell
+  attr_accessor :heading_level
+
+  def initialize(content: '', heading_level: 0)
+    super(content: content, type: 'markdown')
+    self.heading_level = heading_level
+    self.hash = make_md_cell(content: content, heading_level: heading_level)
+  end
+  
+  def make_md_cell(content: '', heading_level: 0)
+    if heading_level.positive?
+      hash_marks = '#' * heading_level
+      self.content = hash_marks + ' ' + content
+    end
+
+    make_cell_hash(type: 'markdown', content: self.content)
+  end
+end
+
+puts MarkdownCell.new(content: "New Notebook", heading_level: 1)

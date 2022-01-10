8cec7779b93873f815b80ae9e6a3fec4a2f2c672
Michael Newman <newman174@gmail.com>
2022-01-10 17:31:07 -0500

    cleanup

--- cell.rb
+++ cell.rb
@@ -1,4 +1,7 @@
-class Cell
+# frozen_string_literal: true
+
+# Cell for Jupyter Notebook
+class NotebookCell
   attr_accessor :type, :content, :hash
 
   def initialize(content: '', type: 'code')
@@ -6,35 +9,23 @@ class Cell
     self.type = type
     self.content = content
   end
-  
+
   # self.make_cell - Valid cell_type are ['code'*, 'markdown'], *default
   # cell = self.make_cell('yo', 'markdown')
   def make_cell_hash(content: '', type: 'code')
-    {"cell_type"=>type,
-      "execution_count"=>0,
-      "metadata"=>{},
-      "outputs"=>[],
-      "source"=>[content]}
+    { 'cell_type' => type,
+      'execution_count' => 0,
+      'metadata' => {},
+      'outputs' => [],
+      'source' => [content] }
   end
-
-  # def make_code_cell(content: '')
-  #   make_cell_hash(type: 'code', content: content)
-  # end
-
-  # def make_md_cell(content: '', heading_level: 0)
-  #   if heading_level.positive?
-  #     hash_marks = '#' * heading_level
-  #     content = hash_marks + ' ' + content
-  #   end
-
-  #   make_cell_hash(type: 'markdown', content: content)
-  # end
 
   def inspect
     details = []
     details << "Type: #{type} Cell"
-    details << "Content:"
-    details << content.lines[0]
+    # details << "Content:"
+    details << "Lines: #{content.lines.size}"
+    details << "First Line: #{content.lines.size}"
     # details << "\n"
     details
   end
@@ -42,12 +33,13 @@ class Cell
   def to_s
     content
   end
-
 end
 
-class CodeCell < Cell; end
+# Code Cells for Notebooks
+class CodeCell < NBCell; end
 
-class MarkdownCell < Cell
+# Markdown Cells for Notebooks
+class MarkdownCell < NBCell
   attr_accessor :heading_level
 
   def initialize(content: '', heading_level: 0)
@@ -55,15 +47,15 @@ class MarkdownCell < Cell
     self.heading_level = heading_level
     self.hash = make_md_cell(content: content, heading_level: heading_level)
   end
-  
+
   def make_md_cell(content: '', heading_level: 0)
     if heading_level.positive?
       hash_marks = '#' * heading_level
-      self.content = hash_marks + ' ' + content
+      self.content = "#{hash_marks} #{content}"
     end
 
     make_cell_hash(type: 'markdown', content: self.content)
   end
 end
 
-puts MarkdownCell.new(content: "New Notebook", heading_level: 1)
+puts MarkdownCell.new(content: 'New Notebook', heading_level: 1)
--- notebook.rb
+++ notebook.rb
@@ -1,59 +1,39 @@
+# frozen_string_literal: true
+
 require '~/ruby_filetools/filetools'
+require '~/Documents/ruby_filetools/filetools'
 require_relative 'cell'
 
+# Jupyter Notebook Tools for Ruby
 class Notebook
   require 'json'
   include FileTools
 
-  # Class Methods
-
   NB_RUBY_VERSION = '3.0.1'
-  
+
   BLANK_NB_HASH = { 'cells' => [],
                     'metadata' => { 'kernelspec' => { 'display_name' => "Ruby #{NB_RUBY_VERSION}",
-                                                     'language' => 'ruby',
-                                                     'name' => 'ruby' },
+                                                      'language' => 'ruby',
+                                                      'name' => 'ruby' },
                                     'language_info' => { 'name' => 'ruby' },
                                     'orig_nbformat' => 4 },
                     'nbformat' => 4,
-                    'nbformat_minor' => 2 }
-                
+                    'nbformat_minor' => 2 }.freeze
+
   def self.to_s
     'Notebook'
   end
 
-  # def self.blank_nb_hash
-  #   { "cells" => [],
-  #     "metadata" => {"kernelspec"=>{"display_name"=>"Ruby 3.0.1", "language"=>"ruby", "name"=>"ruby"}, "language_info"=>{"file_extension"=>".rb", "mimetype"=>"application/x-ruby", "name"=>"ruby", "version"=>"3.0.1"}, "orig_nbformat"=>4},
-  #     "nbformat" => 4,
-  #     "nbformat_minor" => 2 }
-  # end
-  
-  # Notebook.load(nb_file_name)
-  # open json nb file and returns a nb_hash
-  def self.open(nb_file_name)
-    nb_hash = File.open(nb_file_name) do |file|
+  # Open JSON nb file and return a Notebook object
+  def self.open(nb_json_file_name)
+    nb_hash = File.open(nb_json_file_name) do |file|
       JSON.parse(file.read)
     end
-    # TODO: automate setting the title variable on open
-    # puts nb_hash
-    self.new(file_name: nb_file_name, nb_hash: nb_hash)
+    new(file_name: nb_file_name, nb_hash: nb_hash)
   end
-  
-  
-  # TODO: Should be in filetools
-  # def self.fn_format(name)
-  #   name = str.downcase.gsub!(' ', '_').gsub!(/\W/,'')
-  # end
-  
-  # def self.append_cell_to_nb_hash!(cell, nb_hash)
-  #   nb_hash['cells'].push(cell)
-  # end
-  
-  # Instance Methods
-  
+
   attr_accessor :title, :cells, :file_name, :created
-  
+
   def initialize(title: 'New Notebook')
     # @nb_hash = self.class::BLANK_NB_HASH
     self.title = title
@@ -62,14 +42,15 @@ class Notebook
     self.created = Time.new
     add_title_cell unless title.nil?
   end
-  
-  def to_json
+
+  def to_json(*_args)
     JSON.generate(nb_hash)
   end
 
-  def save(file_name: self.file_name, dir: Dir.pwd, folder: false)
+  # def save(file_name: self.file_name, dir: Dir.pwd, folder: false)
+  def save(file_name: self.file_name, dir: Dir.pwd)
     Dir.mkdir(dir) unless Dir.exist?(dir)
-    
+
     file_name += '.ipynb' unless file_name.end_with?('.ipynb')
 
     Dir.chdir(dir) do
@@ -79,15 +60,15 @@ class Notebook
       end
     end
   end
-  
+
   # Add a cell to the notebook.
   def add_cell(cell = Cell.new)
-    cell = Cell.new(content: cell) unless cell.class == Cell
+    cell = Cell.new(content: cell) unless cell.instance_of?(Cell)
     cells << cell
   end
 
-  alias_method :<<, :add_cell
-  alias_method :push, :add_cell
+  alias << add_cell
+  alias push add_cell
 
   def add_title_cell
     add_cell(content: title, heading_level: 1)
@@ -114,14 +95,15 @@ class Notebook
   end
 
   def to_h
-    @nb_hash['cells'] = cells.map { |cell| cell.hash}
+    @nb_hash['cells'] = cells.map(&:hash)
+    @nb_hash
   end
 
   def inspect
     output = []
     output << "Title: #{title}\n\n"
-    output << "Cells:"
-    cells.each_with_index do |cell, index| 
+    output << 'Cells:'
+    cells.each_with_index do |cell, index|
       output << "#{index + 1}. #{cell.type.capitalize} Cell"
       output << cell.content
     end
